39fe214f24ad144303722bc74a6a99ea263627e9
joão pedro keidann <joao_keidann@estudante.sesisenai.org.br>
2026-06-02 08:51:23 -0300

    first commit

---
+++ models/obj.js
@@ -0,0 +1,20 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+        this.img = new Image()
+        this.img.src = a
+    
+        this.hitbox = {
+            x: 0,
+            y: 0,
+            w: w,
+            h: h
+        }
+    }
+}
+
+module.exports = Obj
