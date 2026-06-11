4885a49e0a6a001884e0d0e75ac9f8220f4284b3
joão pedro keidann <joao_keidann@estudante.sesisenai.org.br>
2026-06-11 10:13:11 -0300

    BETA 0.1.1

--- index.js
+++ index.js
@@ -19,7 +19,34 @@ des.imageSmoothingEnabled = false;
 des.webkitImageSmoothingEnabled = false;
 des.mozImageSmoothingEnabled = false;
 
-let player = new Player(200, 200, 128, 128, "../img/images.jpg")
+let player = new Player(200, 200, 128, 128, "../img/killer_bean.png")
+
+// ==================== Movimento jogador =======================
+const keys = {}
+
+let jogar = true
+let fase = 1
+
+let velocidadeCar = 1
+
+document.addEventListener('keydown', (e) => {
+    keys[e.key] = true
+})
+
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false
+})
+
+function controlarPlayers() {
+    player.dirX = 0
+    player.dirY = 0
+
+    if (keys['w']) player.dirY = -1
+    if (keys['s']) player.dirY = 1
+    if (keys['a']) player.dirX = -1
+    if (keys['d']) player.dirX = 1
+}
+
 
 function desenha() {
     player.des_player()
@@ -32,6 +59,7 @@ function atualiza() {
     let limiteDir = canvas.width
 
     player.mov_player(limiteCima, limiteBaixo, limiteEsq, limiteDir)
+    controlarPlayers()
 }
 
 function main() {
